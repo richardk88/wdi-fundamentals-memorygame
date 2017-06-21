56b26f626405d9ed6dcd2c0b7dc0c47c7cf91117
Richard Kim <richardk88@hotmail.com>
2017-06-21 17:01:21 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -4,21 +4,17 @@ var cardsInPlay = [];
 
 var checkForMatch = function(){
 	  if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert ("You found a match!");
   } else {
-      console.log("Sorry, try again.");
+      alert ("Sorry, try again.");
   }
-}
+};
 
 var flipCard = function (cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert ("You found a match!");
-		} else {
-			alert ("Sorry, try again.");
-		}
-		cardsInPlay.push(cards[cardId]);
-		console.log("User flipped " + cards[cardId]);
+		checkForMatch();
 	};
 };
 
